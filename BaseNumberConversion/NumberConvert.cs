ada564f2aa25c08e21ed016eddf9a1a6145b69cd
Cameron <cameroncox007@hotmail.com>
2020-04-15 09:03:29 -0700

    Add project files.

---
+++ BaseNumberConversion/NumberConvert.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseNumberConversion
+{
+    class NumberConvert
+    {
+        public static string dec2bin(int number)
+        {
+            string current = "";
+            do
+            {
+                int nextDigit = number % 2;
+                number /= 2;
+                int digitCode = '0' + nextDigit;
+                char digit = Convert.ToChar(digitCode);
+                current = digit + current;
+            }
+            while (number != 0);
+
+            return current;
+        }
+
+        public static string dec2oct(int number)
+        {
+            string current = "";
+            do
+            {
+                int nextDigit = number % 8;
+                number /= 8;
+                int digitCode = '0' + nextDigit;
+                char digit = Convert.ToChar(digitCode);
+                current = digit + current;
+            }
+            while (number != 0);
+
+            return current;
+        }
+
+        public static int bin2dec(int number)
+        {
+            int decimalValue = 0;
+            int base1 = 1;
+
+            while (number > 0)
+            {
+                int reminder = number % 10;
+                number /= 10;
+                decimalValue += reminder * base1;
+                base1 *= 2;
+            }
+
+            return decimalValue;
+        }
+
+        public static int bin2oct(int number)
+        {
+            int n1, p = 1;
+            int dec = 0, i = 1, j, d;
+            int ocno = 0;
+
+            n1 = number;
+            for (j = number; j > 0; j /= 10)
+            {
+                d = j % 10;
+                if (i == 1)
+                {
+                    p *= 1;
+                }
+                else
+                {
+                    p *= 2;
+                }
+                dec = dec + (d * p);
+                i++;
+            }
+
+            i = 1;
+
+            for (j = dec; j > 0; j /= 8)
+            {
+                ocno = ocno + (j % 8) * i;
+                i *= 10;
+                number /= 8;
+            }
+            return ocno;
+        }
+
+        public static int oct2bin(int number)
+        {
+            int n5 = number, p = 1;
+            int dec = 0, i = 1, j, d;
+            int binno = 0;
+            
+            for (j = number; j > 0; j /= 10)
+            {
+                d = j % 10;
+                if (i == 1)
+                {
+                    p *= 1;
+                }
+                else
+                {
+                    p *= 8;
+                }
+
+                dec = dec + (d * p);
+                i++;
+            }
+
+            i = 1;
+
+            for (j = dec; j > 0; j = j / 2)
+            {
+                binno = binno + (dec % 2) * i;
+                i *= 10;
+                dec /= 2;
+            }
+            return binno;
+        }
+
+        public static int oct2dec(int number)
+        {
+            int decValue = 0;
+            int base1 = 1;
+
+            int temp = number;
+            while (temp > 0)
+            {
+                int lastDigit = temp % 10;
+                temp /= 10;
+                decValue += lastDigit * base1;
+                base1 *= 8;
+            }
+
+            return decValue;
+        }
+    }
+}
---
+++ BaseNumberConversion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BaseNumberConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the integer to convert: ");
+            string n1 = Console.ReadLine();
+            int number = int.Parse(n1);
+
+            Console.Write("Please enter the base to convert from | 2 | 8 | 10 |: ");
+            string n2 = Console.ReadLine();
+            int from = int.Parse(n2);
+
+            Console.WriteLine($"Number: {number}, base: {from}");
+
+            int result = 0;
+            string answer;
+            if (from == 10)
+            {
+                answer = NumberConvert.dec2bin(number);
+                Console.WriteLine($"binary conversion is {answer}");
+                answer = NumberConvert.dec2oct(number);
+                Console.WriteLine($"octal conversion is {answer}");
+            }
+            else if (from == 2)
+            {
+                result = NumberConvert.bin2dec(number);
+                Console.WriteLine($"decimal conversion is {result}");
+                result = NumberConvert.bin2oct(number);
+                Console.WriteLine($"octal conversion is {result}");
+            }
+            else if (from == 8)
+            {
+                result = NumberConvert.oct2bin(number);
+                Console.WriteLine($"binary conversion is {result}");
+                result = NumberConvert.oct2dec(number);
+                Console.WriteLine($"decimal conversion is {result}");
+            }
+            else
+            {
+                Console.WriteLine("Error in base to convert from");
+            }
+        }
+    }
+}
