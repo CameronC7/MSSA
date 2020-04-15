a37c7539d744a4f46847a8b976652ef1c8ca4f0e
Cameron <cameroncox007@hotmail.com>
2020-04-15 08:44:57 -0700

    Add project files.

---
+++ EncryptionAndAuthentication/Account.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionAndAuthentication
+{
+    class Account
+    {
+        public static Dictionary<string, byte[]> accounts = new Dictionary<string, byte[]>();
+        public static Dictionary<string, byte[]> MakeAccount()
+        {
+            string accountName;
+            string password;
+            Console.WriteLine("\tAccount Creation Page");
+            Console.WriteLine("\t---------------------\n");
+            Console.Write("Enter your username: ");
+            accountName = Console.ReadLine();
+            while (accounts.ContainsKey(accountName))
+            {
+                Console.WriteLine("The username you entered already exists!");
+                Console.Write("Please enter another username: ");
+                accountName = Console.ReadLine();
+            }
+            Console.Write("Enter your password: ");
+            password = Console.ReadLine();
+            byte[] encryptedPassword = HashString(password);
+            accounts.Add(accountName, encryptedPassword);
+            Console.WriteLine("\nYour account has been created!");
+            Console.WriteLine("Press any key to go to the main menu");
+            Console.ReadKey();
+            return accounts;
+        }
+
+        public static void ValidateAccount()
+        {
+            string accountName;
+            string password;
+            Console.WriteLine("\tAccount Validation Page");
+            Console.WriteLine("\t---------------------\n");
+            Console.Write("Enter your username name: ");
+            accountName = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            password = Console.ReadLine();
+            byte[] encryptedPassword = HashString(password);
+            if (accounts.ContainsKey(accountName) == true)
+            {
+                for (int i = 0; i < encryptedPassword.Length; i++)
+                {
+                    if (encryptedPassword[i] != accounts.GetValueOrDefault(accountName)[i])
+                    {
+                        Console.WriteLine("The username or password you entered doesn't match any combination in our system. Please try again");
+                        ValidateAccount();
+                    }
+                }
+                Console.WriteLine("\nYour account has been authorized");
+                Console.Write("Press any key to go to the main menu");
+                Console.ReadKey();
+                
+            }
+            else
+            {
+                Console.WriteLine("The username or password you entered doesn't match any combination in our system. Please try again");
+                ValidateAccount();
+            }
+        }
+
+        public static byte[] HashString(string s)
+        {
+            byte[] encryptedString;
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                UnicodeEncoding ue = new UnicodeEncoding();
+                byte[] stringBytes = ue.GetBytes(s);
+                encryptedString = mySHA256.ComputeHash(stringBytes);
+            }
+            return encryptedString;
+        }
+
+        public static void PrintEncryption()
+        {
+            Console.WriteLine("\nHere are all of the stored accounts\n");
+            foreach (KeyValuePair<string, byte[]> element in accounts)
+            {
+                string username = element.Key;
+                Console.WriteLine($"\nUsername: {username}");
+                Console.Write("Password: ");
+                byte[] password = element.Value;
+                foreach (var item in password)
+                {
+                    Console.Write(item);
+                }
+            }
+        }
+    }
+}
---
+++ EncryptionAndAuthentication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EncryptionAndAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag == true)
+            {
+                Console.Clear();
+                int choice = MainMenu();
+
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Account.MakeAccount();
+                }
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    Account.ValidateAccount();
+                }
+                else if (choice == 3)
+                {
+                    Account.PrintEncryption();
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static int MainMenu()
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("\n\tPASSWORD AUTHENTICATION SYSTEM\n");
+            Console.WriteLine("\tPlease select one option:");
+            Console.WriteLine("\t1. Establish an account");
+            Console.WriteLine("\t2. Authenticate a user");
+            Console.WriteLine("\t3. Exit the system\n");
+            Console.WriteLine("\n-----------------------------------------------\n");
+            Console.Write("\tEnter selection: ");
+            int selection = Int32.Parse(Console.ReadLine());
+            return selection;
+        }
+    }
+}
